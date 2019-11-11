c51666c4b19b94f8e5ab79bc53b3fba0a046a23f
Nadeem Anvaritafti <nanvari@live.com>
2019-11-11 14:02:54 -0600

    added tests for Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+
+import React from 'react';
+import {render} from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+test('contains Controls component', () => {
+    const {getByTestId} = render(<Dashboard />);
+    getByTestId(/controls/i);
+});
+
+test('contains Display component', () => {
+    const {getByTestId} = render(<Dashboard />);
+    getByTestId(/display/i);
+});
